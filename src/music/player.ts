688e934d69463eb759011769461af84f3171eff6
Roko Musulin <roko.musulin@gmail.com>
2023-06-01 19:59:36 +0200

    Update dependencies

--- src/music/player.ts
+++ src/music/player.ts
@@ -14,10 +14,8 @@ export class Player extends AudioPlayer {
 		const channelId = voiceConnection.joinConfig.channelId;
 		const channel = global.client.channels?.cache?.get(channelId);
 
-		const stream = ytdl(songId, {quality: 'highestaudio', filter: (format) => {
-				return channel?.bitrate ? format.bitrate <= channel.bitrate : false;
-			}
-		});
+		const stream = ytdl(songId, {quality: 'highest', filter: 'audioonly'});
+
 		const resource = createAudioResource(stream);
 		voiceConnection.subscribe(this);
 		this.play(resource);
